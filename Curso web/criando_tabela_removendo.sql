55e12b84eb05d182d02d59d94be53dbbcb33c666
Matheus Hora <matheus11hora@gmail.com>
2022-02-28 10:42:43 -0300

    Estudo

---
+++ Curso web/criando_tabela_removendo.sql
@@ -0,0 +1,4 @@
+-- Criando tabelas
+CREATE DATABASE db_curso_web;
+-- Apagando banco de dado
+DROP DATABASE db_curso_web;
---
+++ Curso web/tipo_de_variavel.sql
@@ -0,0 +1,13 @@
+--Campo de texto:
+--text(tamanho variavel que armazena uma grande quantidade de caracteres)
+--varchar(tamanho variavel que armazena de 0 a 255 caracteres)
+--char(tamanho fixo que armazena de 0 a 255 caracteres)
+
+--Campos numericos:
+--int(valores numericos inteiros)
+--float(valores numericos fracionados)
+
+--Campos de data e hora:
+--date(yyyy/mm/dd)
+--time(hora)
+--datatime(date e time no mesmo tempos)
